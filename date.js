db69efba4fc909ff444734faa8683365c8d14d26
William <wgenard94@gmail.com>
2021-06-29 14:57:27 +0200

    Date finish

--- date.js
+++ date.js
@@ -8,7 +8,7 @@ let msg = ""
 
 console.log(date)
 
-if (isNaN(date.getTime())) {
+if (!input.match(/^([0-2][0-9]|(3)[0-1])(-)(((0)[0-9])|((1)[0-2]))(-)\d{4}$/)) {
     msg = "La date insérée est incorrecte"
 }
 else {
